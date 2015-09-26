375bc46a1f6d691269e02321d872eb78cb3da188
qiufengilove <6483478@qq.com>
2015-09-27 01:00:03 +0800

    更新示例

--- Lw.RouteTest/Program.cs
+++ Lw.RouteTest/Program.cs
@@ -13,25 +13,33 @@ namespace Lw.RouteTest
     {
         static void Main(string[] args)
         {
+            //匹配自定义规则
             RouteHelper.RegistRoute("/u-test/{action}-{id:int}/{name:long}");
+            //可空的Email
             RouteHelper.RegistRoute("/d-test/{action}/{name:email?}");
+            //可空的QQ
             RouteHelper.RegistRoute("/n-test/{action}/{name:qq?}");
+            //可空的Id
             RouteHelper.RegistRoute("/u-test/{action}/{id:int?}");
+            //自定义正则表达式(只接受字母)
+            RouteHelper.RegistRoute("/u-regex/{action}/{name:^[a-zA-Z]+$}");
             int count = 1000000;
-            var testUrl = "/u-test/mytest/3000";
+            //var testUrl = "/u-test/mytest/3000";
+            var testUrl = "/u-regex/mytest/afdAW";
             IDictionary<string, object> dict = null;
             var timecount = System.Diagnostics.Stopwatch.StartNew();
+            bool result=false;
             for (int i=0;i< count; i++)
             {
-                RouteHelper.IsMatch(testUrl, out dict);
+                result=RouteHelper.IsMatch(testUrl, out dict);
             }
             timecount.Stop();
             Console.WriteLine("测试"+ count +"次匹配，消耗时间:"+ timecount.ElapsedMilliseconds+"毫秒");
-            Console.WriteLine(dict.Count);
             Debug.Assert(dict != null && dict.ContainsKey("action") && dict["action"].ToString() == "mytest", "测试检索action失败");
-            Debug.Assert(dict != null && dict.ContainsKey("id") && dict["id"].ToString() == "3000", "测试检索Id失败");
+            Debug.Assert(dict != null && dict.ContainsKey("name") && dict["name"].ToString() == "3000", "测试检索Id失败");
 
-            Console.WriteLine(dict["action"]+"                "+dict["id"]);
+            Console.WriteLine(result.ToString());
+            //Console.WriteLine(dict["action"]+"                "+dict["id"]);
         }
     }
 }
